1c4de311b2b13f32374ab169f29147faa34f30e5
Pieter withaar <pieter.withaar@improveqs.nl>
2025-02-19 21:26:36 +0100

    some colors and half telnet

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,26 @@
 #include <Arduino.h>
-
-#include <Lpf2Hub.h>      //legoino
-#include <Lpf2HubConst.h> //legoino
-#include <Bounce2.h>      //bounce2
-
-#include <Adafruit_GFX.h>
-#include <Adafruit_NeoMatrix.h>
-#include <Adafruit_NeoPixel.h>
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <ArduinoOTA.h>
-
+#include <Lpf2Hub.h>      //legoino
+#include <Lpf2HubConst.h> //legoino
+#include <Bounce2.h>      //bounce2
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#include "esp_log.h"
 
 #define WIFI_SSID "KruItbOsT"
 #define WIFI_PASS "zukweg-Zidpa5"
 
 WiFiMulti WiFiMulti;
+WiFiServer telnetServer(23);
+WiFiClient telnetClient;
 
 Lpf2Hub myHub;
 byte port = (byte)PoweredUpHubPort::A;
 
-//Pin declaration
+// Pin declaration
 #define BTN_MUSIC 1
 #define BTN_LIGHT 2
 #define BTN_WATER 3
@@ -35,15 +35,18 @@ Bounce pbMusic = Bounce();
 Bounce pbLight = Bounce();
 Bounce pbWater = Bounce();
 Bounce pbStop = Bounce();
+Bounce pbBlack = Bounce();
+
 Bounce pbDirectionSwitch = Bounce();
 Bounce pbSpeedModeSwitch = Bounce();
 
+bool TrainStopped = true;
 bool DirectionSwitchState = true;
 int gLastStatePtiSpeed = 0;
 
 static int gLightOn = 0;
 static short gColor = NONE;
-static int gSpeed =0;
+static int gSpeed = 0;
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, LED_MATRIX,
   NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
@@ -52,12 +55,25 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, LED_MATRIX,
 
 const uint16_t colors[] = {
     matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
-  
+
+bool useTelnet = true;
+
+int customLogFunction(const char *fmt, va_list args) {
+  char buffer[256];
+  int len = vsnprintf(buffer, sizeof(buffer), fmt, args);
+  if (useTelnet && telnetClient && telnetClient.connected()) {
+    telnetClient.write(buffer, len);
+  } else {
+    Serial.write(buffer, len);
+  }
+  return len;
+}
+
 void handlePoti()
 {
   int ptiSpeed = analogRead(PTI_SPEED);
   gLastStatePtiSpeed = ptiSpeed;
-
+  ESP_LOGI("ptiSpeed", "ptiSpeed: %d", ptiSpeed);
 
   int speed = 0;
   // Serial.print("PTI Speed: ");
@@ -87,47 +103,56 @@ void handlePoti()
     // Check the state of pbDirectionSwitch to determine if speed should be positive or negative
     if (DirectionSwitchState) {
       // Positive speed
-      Serial.println("Direction switch is HIGH, setting positive speed");
+      ESP_LOGI("Direction", "Direction switch is HIGH, setting positive speed");
     } else {
       // Negative speed
       speed = -speed;
-      Serial.println("Direction switch is LOW, setting negative speed");
+      ESP_LOGI("Direction", "Direction switch is LOW, setting negative speed");
     }
 
   }
 
   if (speed != gSpeed)
   { 
-    // Serial.print("Speed changed to: ");
-    // Serial.print(ptiSpeed);
-    // Serial.print(" -> ");
-    // Serial.println(speed);
+    ESP_LOGI("Speed", "Speed changed to: %d", speed);
+    if (speed == 0)
+    {
+      TrainStopped = false;
+      matrix.fillScreen(matrix.Color(0, 0, 0)); // Red
+
+    }
+
     if (gSpeed == 0 && speed > 0)
     {
+      
         myHub.playSound((byte)DuploTrainBaseSound::STATION_DEPARTURE);
         delay(100);
     }
     gSpeed = speed;
-    myHub.setBasicMotorSpeed(port, speed);
+
+    if (!TrainStopped)
+    {
+      myHub.setBasicMotorSpeed(port, speed);
+    }
+    
   }
 }
-
 Color getNextColor()
 {
     if(gColor == 255)
-    { 
+    {
         gColor = 0;
         return (Color)0;
     }
-       
+
     gColor++;
-    
+
     if(gColor == NUM_COLORS)
-    {   
-        gColor =255;
+    {
+        gColor = 255;
     }
     return (Color) gColor;
-} 
+}
 
 void handleButtons()
 {
@@ -136,18 +161,18 @@ void handleButtons()
     if (pbDirectionSwitch.fell())
     {
       DirectionSwitchState = !DirectionSwitchState;
-      Serial.println("Direction Change");
-
+      ESP_LOGI("Button", "Direction Change");
+      matrix.fillScreen(matrix.Color(0, 255, 0)); // Green
     }
   }
   if (pbSpeedModeSwitch.update())
   {
     if (pbSpeedModeSwitch.fell())
     {
-      Serial.println("Speed Modus Changed High");
+      ESP_LOGI("Button", "Speed Modus Changed High");
     } else
     {
-      Serial.println("Speed Modus Changed Low");
+      ESP_LOGI("Button", "Speed Modus Changed Low");
     }
   }
   if(pbMusic.update())
@@ -156,6 +181,7 @@ void handleButtons()
     {
       myHub.playSound((byte)DuploTrainBaseSound::HORN);
       delay(100);
+      matrix.fillScreen(matrix.Color(255, 255, 0)); // Yellow
     }
   }
   if(pbLight.update())
@@ -164,6 +190,7 @@ void handleButtons()
     {
       myHub.setLedColor(getNextColor());
       delay(100);
+      matrix.fillScreen(matrix.Color(255, 255, 255)); // White
     }
   }
   if(pbWater.update())
@@ -171,23 +198,33 @@ void handleButtons()
     if(pbWater.fell())
     {
       myHub.playSound((byte)DuploTrainBaseSound::WATER_REFILL);
-      delay(100);  
+      delay(100);
+      matrix.fillScreen(matrix.Color(0, 0, 255)); // Blue
     }
   }
   if(pbStop.update())
   {
     if(pbStop.fell())
     {
-      myHub.setBasicMotorSpeed(port, 0);
-      delay(100);
+      myHub.setBasicMotorSpeed(port, 0);      
+      TrainStopped = true;
       myHub.playSound((byte)DuploTrainBaseSound::BRAKE);
+      matrix.fillScreen(matrix.Color(255, 0, 0)); // Red
+      delay(100);      
+    }
+  }
+  if(pbBlack.update())
+  {
+    if(pbBlack.fell())
+    {
+      matrix.fillScreen(matrix.Color(255, 165, 0)); // Orange
     }
   }
 }
 
 void setup() {
   Serial.begin(115200);
-  delay(10);  
+  delay(10);
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setBrightness(10);
@@ -217,8 +254,6 @@ void setup() {
     }
   }
 
-
-
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.print("SSID: ");
@@ -230,13 +265,12 @@ void setup() {
   Serial.print("Hostname: ");
   Serial.println(WiFi.getHostname());
 
-  // OTA Configiration and Enable OTA
+  // OTA Configuration and Enable OTA
   Serial.println("\nEnabling OTA Feature");
   ArduinoOTA.setPassword("duplotrain");
   ArduinoOTA.begin();
 
-  //define Pin Modes
-
+  // Define Pin Modes
   pbMusic.attach(BTN_MUSIC, INPUT_PULLUP);
   pbLight.attach(BTN_LIGHT, INPUT_PULLUP);
   pbWater.attach(BTN_WATER, INPUT_PULLUP);
@@ -244,10 +278,18 @@ void setup() {
   pbDirectionSwitch.attach(BTN_DIRECTION, INPUT_PULLUP);
   pbSpeedModeSwitch.attach(BTN_SWITCH, INPUT_PULLUP);
 
-  
-  
+  // Start Telnet server
+  telnetServer.begin();
+  telnetServer.setNoDelay(true);
+
   Serial.println("Init Done");
   myHub.init();
+
+  // Set custom log function
+  esp_log_set_vprintf(customLogFunction);
+
+  // Set log level to INFO
+  esp_log_level_set("*", ESP_LOG_INFO);
 }
 
 int x    = matrix.width();
@@ -255,27 +297,40 @@ int pass = 0;
 
 void loop() {
   ArduinoOTA.handle();
-    if (myHub.isConnecting()) {
-        myHub.connectHub();
-        if (myHub.isConnected()) {
-            Serial.println("We are now connected to the HUB");
-        } else {
-            Serial.println("We have failed to connect to the HUB");
-        }
-    }
-    if (myHub.isConnected()) {
-        handleButtons();
-        handlePoti();
-    } 
 
-    matrix.fillScreen(0);
-    matrix.setCursor(x, 0);
-    matrix.print(F("LEGO Duplo Train Controller"));
-    if(--x < -216) {
-      x = matrix.width();
-      if(++pass >= 3) pass = 0;
-      matrix.setTextColor(colors[pass]);
+  // Handle Telnet client
+  if (telnetServer.hasClient()) {
+    if (!telnetClient || !telnetClient.connected()) {
+      if (telnetClient) telnetClient.stop();
+      telnetClient = telnetServer.available();
+      telnetClient.flush();
+      telnetClient.println("Connected to DuploTrainController");
     }
-    matrix.show();
-    delay(50);
+  }
+
+  if (myHub.isConnecting()) {
+    myHub.connectHub();
+    if (myHub.isConnected()) {
+      ESP_LOGI("Hub", "We are now connected to the HUB");
+      matrix.fillCircle(4, 4, 3, matrix.Color(0, 255, 0));
+    } else {
+      ESP_LOGI("Hub", "We have failed to connect to the HUB");
+      matrix.fillCircle(4, 4, 3, matrix.Color(255, 0, 0));
+    }
+  }
+  if (myHub.isConnected()) {
+    handleButtons();
+    handlePoti();
+  }
+
+  // matrix.fillScreen(0);
+  // matrix.setCursor(x, 0);
+  // matrix.print(F("LEGO Duplo Train Controller"));
+  // if (--x < -216) {
+  //   x = matrix.width();
+  //   if (++pass >= 3) pass = 0;
+  //   matrix.setTextColor(colors[pass]);
+  // }
+  matrix.show();
+  delay(50);
 }
