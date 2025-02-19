dcea76f560a181198f2cf53d79b2d213cf55fdb6
Pieter withaar <pieter.withaar@improveqs.nl>
2025-02-19 22:40:13 +0100

    made stop stop

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
-#include "esp_log.h"
 
 #define WIFI_SSID "KruItbOsT"
 #define WIFI_PASS "zukweg-Zidpa5"
@@ -16,6 +15,12 @@
 WiFiMulti WiFiMulti;
 WiFiServer telnetServer(23);
 WiFiClient telnetClient;
+bool useTelnet = true;
+
+
+
+
+
 
 Lpf2Hub myHub;
 byte port = (byte)PoweredUpHubPort::A;
@@ -34,7 +39,7 @@ byte port = (byte)PoweredUpHubPort::A;
 Bounce pbMusic = Bounce();
 Bounce pbLight = Bounce();
 Bounce pbWater = Bounce();
-Bounce pbStop = Bounce();
+Bounce2::Button pbStop = Bounce2::Button();
 Bounce pbBlack = Bounce();
 
 Bounce pbDirectionSwitch = Bounce();
@@ -56,16 +61,32 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, LED_MATRIX,
 const uint16_t colors[] = {
     matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
 
-bool useTelnet = true;
+static void logToAll(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+
+  char buffer[256];
+  vsnprintf(buffer, sizeof(buffer), fmt, args);
+  va_end(args);
+
+  // Output via Telnet
+  if (useTelnet && telnetClient && telnetClient.connected()) {
+    telnetClient.println(buffer);
+  }
+
+  // Also output via Serial
+  Serial.println(buffer);
+}
 
 int customLogFunction(const char *fmt, va_list args) {
   char buffer[256];
   int len = vsnprintf(buffer, sizeof(buffer), fmt, args);
-  if (useTelnet && telnetClient && telnetClient.connected()) {
+  if (buffer[len-1] != '\n') buffer[len++] = '\n';
+  //if (useTelnet && telnetClient && telnetClient.connected()) {
     telnetClient.write(buffer, len);
-  } else {
-    Serial.write(buffer, len);
-  }
+  // } else {
+  //   Serial.write(buffer, len);
+  // }
   return len;
 }
 
@@ -73,7 +94,7 @@ void handlePoti()
 {
   int ptiSpeed = analogRead(PTI_SPEED);
   gLastStatePtiSpeed = ptiSpeed;
-  ESP_LOGI("ptiSpeed", "ptiSpeed: %d", ptiSpeed);
+  //logToAll("ptiSpeed: %d", ptiSpeed);
 
   int speed = 0;
   // Serial.print("PTI Speed: ");
@@ -103,18 +124,18 @@ void handlePoti()
     // Check the state of pbDirectionSwitch to determine if speed should be positive or negative
     if (DirectionSwitchState) {
       // Positive speed
-      ESP_LOGI("Direction", "Direction switch is HIGH, setting positive speed");
+      logToAll("Direction switch is HIGH, setting positive speed");
     } else {
       // Negative speed
       speed = -speed;
-      ESP_LOGI("Direction", "Direction switch is LOW, setting negative speed");
+      logToAll("Direction switch is LOW, setting negative speed");
     }
 
   }
 
   if (speed != gSpeed)
   { 
-    ESP_LOGI("Speed", "Speed changed to: %d", speed);
+    logToAll("Speed changed to: %d", speed);
     if (speed == 0)
     {
       TrainStopped = false;
@@ -130,11 +151,12 @@ void handlePoti()
     }
     gSpeed = speed;
 
-    if (!TrainStopped)
+    if (TrainStopped)
     {
+      myHub.stopBasicMotor(port);
+    } else {
       myHub.setBasicMotorSpeed(port, speed);
     }
-    
   }
 }
 Color getNextColor()
@@ -154,6 +176,23 @@ Color getNextColor()
     return (Color) gColor;
 }
 
+uint16_t getColorRGB(Color color) {
+  switch (color) {
+      case BLACK: return matrix.Color(0, 0, 0);
+      case PINK: return matrix.Color(255, 192, 203);
+      case PURPLE: return matrix.Color(128, 0, 128);
+      case BLUE: return matrix.Color(0, 0, 255);
+      case LIGHTBLUE: return matrix.Color(173, 216, 230);
+      case CYAN: return matrix.Color(0, 255, 255);
+      case GREEN: return matrix.Color(0, 255, 0);
+      case YELLOW: return matrix.Color(255, 255, 0);
+      case ORANGE: return matrix.Color(255, 165, 0);
+      case RED: return matrix.Color(255, 0, 0);
+      case WHITE: return matrix.Color(255, 255, 255);
+      default: return matrix.Color(0, 0, 0);
+  }
+}
+
 void handleButtons()
 {
   if (pbDirectionSwitch.update())
@@ -161,7 +200,7 @@ void handleButtons()
     if (pbDirectionSwitch.fell())
     {
       DirectionSwitchState = !DirectionSwitchState;
-      ESP_LOGI("Button", "Direction Change");
+      logToAll("Direction Change");
       matrix.fillScreen(matrix.Color(0, 255, 0)); // Green
     }
   }
@@ -169,10 +208,10 @@ void handleButtons()
   {
     if (pbSpeedModeSwitch.fell())
     {
-      ESP_LOGI("Button", "Speed Modus Changed High");
+      logToAll("Speed Modus Changed High");
     } else
     {
-      ESP_LOGI("Button", "Speed Modus Changed Low");
+      logToAll("Speed Modus Changed Low");
     }
   }
   if(pbMusic.update())
@@ -188,9 +227,9 @@ void handleButtons()
   {
     if(pbLight.fell())
     {
-      myHub.setLedColor(getNextColor());
-      delay(100);
-      matrix.fillScreen(matrix.Color(255, 255, 255)); // White
+      Color color = getNextColor();
+      myHub.setLedColor(color);      
+      matrix.fillScreen(getColorRGB(color));
     }
   }
   if(pbWater.update())
@@ -204,13 +243,21 @@ void handleButtons()
   }
   if(pbStop.update())
   {
-    if(pbStop.fell())
+  
+
+    if(pbStop.pressed())
     {
-      myHub.setBasicMotorSpeed(port, 0);      
-      TrainStopped = true;
-      myHub.playSound((byte)DuploTrainBaseSound::BRAKE);
-      matrix.fillScreen(matrix.Color(255, 0, 0)); // Red
-      delay(100);      
+      telnetClient.println("Stopped");
+      myHub.stopBasicMotor(port);
+      if (gSpeed != 0)
+      {
+        TrainStopped = true;
+        myHub.playSound((byte)DuploTrainBaseSound::BRAKE);
+        matrix.fillScreen(matrix.Color(255, 0, 0)); // Red
+      } else
+      {
+        TrainStopped = false;
+      }      
     }
   }
   if(pbBlack.update())
@@ -224,6 +271,10 @@ void handleButtons()
 
 void setup() {
   Serial.begin(115200);
+  // Set custom log function
+  esp_log_set_vprintf(customLogFunction);
+  esp_log_level_set("*", ESP_LOG_VERBOSE);
+  logToAll("Custom log function test");
   delay(10);
   matrix.begin();
   matrix.setTextWrap(false);
@@ -233,12 +284,12 @@ void setup() {
   matrix.setPixelColor(0, 0, 0, 255);
   matrix.show();
 
-  Serial.println("Init Start");
+  logToAll("Init Start");
   WiFi.setHostname("DuploTrainController");
 
   WiFiMulti.addAP(WIFI_SSID, WIFI_PASS);
 
-  Serial.print("\n\nWaiting for WiFi... ");
+  logToAll("\n\nWaiting for WiFi... ");
 
   // WIFI Connection, Reboot after 30 attempts
   uint8_t not_connected_counter = 0;
@@ -249,12 +300,12 @@ void setup() {
     not_connected_counter++;
     if (not_connected_counter > 30)
     {
-      Serial.println("Resetting due to Wifi not connecting...");
+      logToAll("Resetting due to Wifi not connecting...");
+      matrix.setPixelColor(0, 255, 0, 0);
       ESP.restart();
     }
   }
 
-  Serial.println("");
   Serial.println("WiFi connected");
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
@@ -264,7 +315,6 @@ void setup() {
   Serial.println(WiFi.macAddress());
   Serial.print("Hostname: ");
   Serial.println(WiFi.getHostname());
-
   // OTA Configuration and Enable OTA
   Serial.println("\nEnabling OTA Feature");
   ArduinoOTA.setPassword("duplotrain");
@@ -275,21 +325,20 @@ void setup() {
   pbLight.attach(BTN_LIGHT, INPUT_PULLUP);
   pbWater.attach(BTN_WATER, INPUT_PULLUP);
   pbStop.attach(BTN_STOP, INPUT_PULLUP);
+  pbStop.interval( 5 );
+  pbStop.setPressedState( HIGH ); 
+
   pbDirectionSwitch.attach(BTN_DIRECTION, INPUT_PULLUP);
   pbSpeedModeSwitch.attach(BTN_SWITCH, INPUT_PULLUP);
 
   // Start Telnet server
   telnetServer.begin();
   telnetServer.setNoDelay(true);
+  matrix.setPixelColor(0, 0, 255, 0);
+  logToAll("Init Done");
+  myHub.init(60000);
 
-  Serial.println("Init Done");
-  myHub.init();
-
-  // Set custom log function
-  esp_log_set_vprintf(customLogFunction);
-
-  // Set log level to INFO
-  esp_log_level_set("*", ESP_LOG_INFO);
+ 
 }
 
 int x    = matrix.width();
@@ -305,16 +354,17 @@ void loop() {
       telnetClient = telnetServer.available();
       telnetClient.flush();
       telnetClient.println("Connected to DuploTrainController");
+      logToAll("Custom log function test");
     }
   }
 
   if (myHub.isConnecting()) {
     myHub.connectHub();
     if (myHub.isConnected()) {
-      ESP_LOGI("Hub", "We are now connected to the HUB");
+      logToAll("We are now connected to the HUB");
       matrix.fillCircle(4, 4, 3, matrix.Color(0, 255, 0));
     } else {
-      ESP_LOGI("Hub", "We have failed to connect to the HUB");
+      logToAll("We have failed to connect to the HUB");
       matrix.fillCircle(4, 4, 3, matrix.Color(255, 0, 0));
     }
   }
